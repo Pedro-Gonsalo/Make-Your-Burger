a8465d7e70c56f7bcb80a5d215fa1bc7afc672da
PedroGonsalo <pedro.henrique@grvsoftware.com.br>
2025-01-30 22:46:43 -0300

    teste bloqueio de git push na branch

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-  import Navbar from "./components/Navbar.vue";
+  import Navbar from "./components/Navbar.vue"; 
   import Footer from "./components/Footer.vue";
 
   export default {
